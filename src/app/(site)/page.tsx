b0f22c6098d5df00d5572c03a8de01cd61c71028
Carolina <carolina.mauro@brosoft.com.ar>
2026-03-01 23:19:21 -0300

    nav bar

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
+          Welcome to Next.js 14!
+        </h1>
+        <p className="text-lg text-gray-700 dark:text-gray-300">
+          This is the default page for the (site) segment.
+        </p>
+      </div>
+    </div>
+  );
+}
